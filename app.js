672ab76b79a6355f7c0ff34cd2e80521e8c22b53
Philippe CHARRIERE <k33g@github.com>
2016-09-09 11:22:36 +0200

    🍄 Try again

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var exec = require('child_process').exec;
 var cmd = './bin/hubot --adapter slack';
+var http = require('http');
+
 
 exec(cmd, (err, stdout, stderr) => {
   if (err) {
@@ -8,3 +10,10 @@ exec(cmd, (err, stdout, stderr) => {
   }
   console.log(stdout);
 });
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello I'm B*B\n");
+});
+
+server.listen(8081);
